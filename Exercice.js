d08ed3c652bc16fa96a3fd059a67f2268da67581
Achref awaissi <achrefawaissi@gmail.com>
2023-02-28 17:49:17 +0100

    first commit et intialissation des constantes

---
+++ Exercice.js
@@ -0,0 +1,18 @@
+const NB_LANCERS = 3;
+const NB_DES = 5;
+const NB_FACES = 6;
+const SCORES_POSSIBLES = [
+  'as',
+  'deux',
+  'trois',
+  'quatre',
+  'cinq',
+  'six',
+  'brelan',
+  'carre',
+  'full',
+  'petiteSuite',
+  'grandeSuite',
+  'yathzee',
+  'chance',
+];
